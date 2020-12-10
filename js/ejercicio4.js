877d8d3d84f1455daa36f3c10a6b81fd4f712899
Pablo Helguera <phelguera33@gmail.com>
2020-12-10 19:22:36 -0300

    se termino el ejercicio N3

---
+++ js/ejercicio4.js
@@ -0,0 +1,6 @@
+//Ejercicio N4
+
+/* Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario» */
+
+let nombreUsuario = prompt('Nombre de usuario');
+document.write(nombreUsuario);
