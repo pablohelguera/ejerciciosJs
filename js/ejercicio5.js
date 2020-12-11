6fb3890dd29de4165c145d0317efe1a239d03e2a
Pablo Helguera <phelguera33@gmail.com>
2020-12-11 16:21:08 -0300

    se termino el ejercicio N5

---
+++ js/ejercicio5.js
@@ -0,0 +1,8 @@
+//Ejercicio N5
+
+/* Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números. */
+
+let a = parseInt(prompt('Ingrese un numero'));
+let b = parseInt(prompt('Ingrese otro numero'));
+
+document.write(a + b)
